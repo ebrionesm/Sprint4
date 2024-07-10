b7b197001da1785debafc60e94ba393affd9eefd
ebrionesm <168421130+ebrionesm@users.noreply.github.com>
2024-07-10 13:12:02 +0200

    Revert "Feature show decks"

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\CardController;
-use App\Http\Controllers\DeckController;
 
-/*Route::get('/', function () {
-    return view('index', [CardController::class, 'index']);
-});*/
-
-route::get('/', [DeckController::class, 'index']);
+Route::get('/', function () {
+    return view('test');
+});
